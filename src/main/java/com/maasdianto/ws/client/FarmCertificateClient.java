78ef85f11b2662237fe797441dd9445ad30a85db
Maas Dianto <maas.dianto@gmail.com>
2012-06-07 10:39:44 +0700

    format result output

--- src/main/java/com/maasdianto/ws/client/FarmCertificateClient.java
+++ src/main/java/com/maasdianto/ws/client/FarmCertificateClient.java
@@ -6,24 +6,23 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
-import com.maasdianto.certificate.FarmCertification;
-import com.maasdianto.certificate.GetFarmCertificationByStatusRequest;
-import com.maasdianto.certificate.GetFarmCertificationByStatusResponse;
+import com.maasdianto.certification.FarmCertification;
+import com.maasdianto.certification.GetFarmCertificationByStatusRequest;
+import com.maasdianto.certification.GetFarmCertificationByStatusResponse;
 
 public class FarmCertificateClient {
 	
 	public static void main(String[] args){
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:client-ws-ctx.xml");
-		WebServiceTemplate template = context.getBean(WebServiceTemplate.class);
+		WebServiceTemplate template = (WebServiceTemplate) context.getBean("webserviceTemplate");
 		
 		GetFarmCertificationByStatusRequest request = new GetFarmCertificationByStatusRequest();
 		request.setStatus("invalid");
 		
 		GetFarmCertificationByStatusResponse response = (GetFarmCertificationByStatusResponse) template.marshalSendAndReceive(request);
 		List<FarmCertification> list = response.getFarmCertification();
-		for (FarmCertification farmCertification : list) {
-			System.out.println(farmCertification.getId());
-			System.out.println(farmCertification.getStatus());
+		for (FarmCertification farm : list) {
+			System.out.println("ID : " + farm.getId() + " , Start Date: " + farm.getStartDate() + ", End Date : " + farm.getEndDate());
 		}
 		
 	}
